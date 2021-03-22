2e8c3e7df6ea3e0a9442693c791c1eae8fbce9af
zhangkj <zhangkj@rayeye.com>
2021-03-22 22:38:19 +0800

    .

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <quick-menu class="quick-menu" :menu-count="count" :icon-class="icons" :menu-url-list="list" @process="handProcess"></quick-menu>
+    <canvas id="canvas" ref="canvas" width="100%" height="100%"></canvas>
+  </div>
+</template>
+
+<script>
+import { fabric } from 'fabric'
+import quickMenu from 'vue-quick-menu'
+export default {
+  name: "App",
+  components: {
+    quickMenu
+  },
+  data(){
+    return {
+      card: null,
+      count: 3,
+      icons:["iconfont icon-xianduan","iconfont icon-tx-tuoyuanxing","iconfont icon-tx-zhengfangxing"],
+      list:[{'isLink':false},{'isLink':false},{'isLink':false}],
+    }
+  },
+  methods:{
+    handProcess(e){
+      console.log(e)
+    },
+    initCanvas(){
+      this.card = new fabric.Canvas('canvas',{
+         backgroundColor: '#fff',
+      });
+      this.card.setWidth(1000)
+      this.card.setHeight(500);
+      this.card.on('mouse:down', (e) => {
+        console.log(e)
+      })
+      this.card.on('mouse:up', (e) => {
+        console.log(e)
+      })
+      // this.card.on('mouse:move', (e) => {
+      //   console.log(e)
+      // })
+    }
+  },
+  mounted(){
+    this.initCanvas();
+  }
+};
+</script>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app {
+  background-image: url('./assets/Explosion_4k.jpg');
+  width: 100%;
+  height: 100%;
+}
+.canvas-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+#canvas{
+  width: 100%;
+  height: 100%;
+}
+.sub-menu{
+  a{
+    display: flex !important;
+    justify-content: center;
+    align-items: center;
+    i{
+      margin-top: 0 !important;
+    }
+  }
+}
+.quick-menu{
+  position: fixed;
+  z-index: 100;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">
+    <canvas id="canvas" width="350" height="200"></canvas>
+  </div>
+</template>
+
+<script>
+import { fabric } from 'fabric'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  mounted(){
+    console.log(process.env)
+    const card = new fabric.Canvas('canvas');
+    card.setWidth(350)
+    card.setHeight(200)
+
+    card.on('object:modified', (e) => {
+        console.log(e.target) // e.target为当前编辑的Object
+        // ...旋转，缩放，移动等编辑图层的操作都监听到
+        // 所以如果有撤销/恢复的场景，这里可以保存编辑状态
+    });
+
+    var rect = new fabric.Rect({
+
+        left:100,//距离画布左侧的距离，单位是像素
+
+        top:100,//距离画布上边的距离
+
+        fill:'red',//填充的颜色
+
+        width:30,//方形的宽度
+
+        height:30//方形的高度
+
+    });
+    card.add(rect);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#canvas{
+  background: #ccc;
+}
+</style>
